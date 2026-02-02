5ab2c5e7317c5d832ebc0dc59b0e52a93b128d09
Matthew Tujague <mjt58@njit.edu>
2026-02-02 04:07:10 -0500

    init admin dash

---
+++ server/db.ts
@@ -0,0 +1,14 @@
+import { Pool } from "pg";
+import { drizzle } from "drizzle-orm/node-postgres";
+
+const databaseUrl = process.env.DATABASE_URL;
+
+if (!databaseUrl) {
+  throw new Error("DATABASE_URL is required");
+}
+
+export const pool = new Pool({
+  connectionString: databaseUrl,
+});
+
+export const db = drizzle(pool);
